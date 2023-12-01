ae4cb9cd205d4c697c71bff1b83740e75736ec0f
gptequality <gptequality@openai.com>
2023-12-01 08:09:58 +0800

    home-server: ipv6-ddns

--- networking/ipv6-ddns.sh
+++ networking/ipv6-ddns.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -x
+set -e
+
 # store the following sensitive information in $HOME/cloudflare.env file with
 # permission of 600
 
@@ -8,22 +11,27 @@
 # export ZONE_ID="your_zone_id"
 # export RECORD_NAME="your_record_name"
 
-source $HOME/cloudflare.env 
+source $HOME/.secrets/cloudflare.env 
 
 # Get the current public IPv6 address
 IP6=`ip -6 addr show dev eth0 | grep global | awk '{print \$2}' | awk -F "/" '{print \$1}'`
 
 # Get the record ID
-RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=AAAA&name=$RECORD_NAME" \
-    -H "X-Auth-Email: $EMAIL" \
-    -H "X-Auth-Key: $API_KEY" \
-    -H "Content-Type: application/json" | jq -r .result[0].id)
+# RECORD_ID=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=AAAA&name=$RECORD_NAME" \
+#     -H "X-Auth-Email: $EMAIL" \
+#     -H "X-Auth-Key: $API_KEY" \
+#     -H "Content-Type: application/json" | jq -r .result[0].id)
 
-# Update the record
-curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records?type=AAAA&name=$RECORD_NAME&content=127.0.0.1&page=1&per_page=100&order=type&direction=desc&match=any" \
     -H "X-Auth-Email: $EMAIL" \
     -H "X-Auth-Key: $API_KEY" \
     -H "Content-Type: application/json" \
-    --data '{"type":"AAAA","name":"'"$RECORD_NAME"'","content":"'"$IP6"'","ttl":120,"proxied":false}' > /dev/null
 
-echo "Updated IPv6 address to $IP6"
+# Update the record
+# curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$ZONE_ID/dns_records/$RECORD_ID" \
+#     -H "X-Auth-Email: $EMAIL" \
+#     -H "X-Auth-Key: $API_KEY" \
+#     -H "Content-Type: application/json" \
+#     --data '{"type":"AAAA","name":"'"$RECORD_NAME"'","content":"'"$IP6"'","ttl":120,"proxied":false}' > /dev/null
+# 
+# echo "Updated IPv6 address to $IP6"
