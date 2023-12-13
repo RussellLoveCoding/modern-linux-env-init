e60f3639992fafbca76533baed6d8711db5cf881
Your Name <you@example.com>
2023-12-13 03:02:16 +0000

    added pve installation

--- installer/server_env.sh
+++ installer/server_env.sh
@@ -283,38 +283,12 @@ installK8s() {
 }
 
 installPVE() {
-    # Debian，Proxmox
-    # 一般情况下，需要同时修改基础系统（Debian）的源文件 /etc/apt/sources.list 和 Proxmox 的源文件。
-    # 修改基础系统（Debian）的源文件，可以使用如下命令：
-
-    cp /etc/apt/sources.list /etc/apt/sources.list.bak
-    sed -i 's|^deb http://ftp.debian.org|deb https://mirrors.ustc.edu.cn|g' /etc/apt/sources.list
-    sed -i 's|^deb http://security.debian.org|deb https://mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list
-
-    # 修改 Proxmox 的源文件，可以使用如下命令：
-    source /etc/os-release
-    echo "deb https://mirrors.ustc.edu.cn/proxmox/debian/pve $VERSION_CODENAME pve-no-subscription" > /etc/apt/sources.list.d/pve-no-subscription.list
-    # 对于 Proxmox Backup Server 和 Proxmox Mail Gateway，请将以上命令中的 pve 分别替换为 pbs 和 pmg。
-    # PVE 8 之后默认安装 ceph 仓库源文件 /etc/apt/sources.list.d/ceph.list，可以使用如下命令更换源：
-
-    if [ -f /etc/apt/sources.list.d/ceph.list ]; then CEPH_CODENAME=`ceph -v | grep ceph | awk '{print $(NF-1)}'`; source /etc/os-release; echo "deb https://mirrors.ustc.edu.cn/proxmox/debian/ceph-$CEPH_CODENAME $VERSION_CODENAME no-subscription" > /etc/apt/sources.list.d/ceph.list; fi
-    apt update
-
-    # CT Templates
-    # 另外，如果你需要使用 Proxmox 网页端下载 CT Templates，可以替换 CT Templates 的源为 http://mirrors.ustc.edu.cn。
-
-    # 具体方法：将 /usr/share/perl5/PVE/APLInfo.pm 文件中默认的源地址 http://download.proxmox.com 替换为 https://mirrors.ustc.edu.cn/proxmox 即可。
-
-    # 可以使用如下命令：
-
-    cp  /usr/share/perl5/PVE/APLInfo.pm /usr/share/perl5/PVE/APLInfo.pm.bak
-    cp /usr/share/perl5/PVE/APLInfo.pm /usr/share/perl5/PVE/APLInfo.pm_back
-    sed -i 's|http://download.proxmox.com|https://mirrors.ustc.edu.cn/proxmox|g' /usr/share/perl5/PVE/APLInfo.pm
-    systemctl restart pvedaemon
-
-    # 针对 /usr/share/perl5/PVE/APLInfo.pm 文件的修改，执行`systemctl restart pvedaemon`后生效。
+    sed -rin 's~(.*)~# \1~g;' /etc/apt/sources.list.d/pve-enterprise.list
+    echo "deb http://download.proxmox.com/debian/pve buster pve-no-subscription" >> /etc/apt/sources.list.d/pve-enterprise.list
+    apt update --allow-insecure-repositories --allow-unauthenticated
+    apt-get install vim lrzsz unzip net-tools curl screen uuid-runtime git -y 
+    apt dist-upgrade -y
 }
-
 installKVM() {
     # 安装所需的软件包：
     $installType bridge-utils cpu-checker libvirt-clients libvirt-daemon qemu qemu-kvm 1>/dev/null
